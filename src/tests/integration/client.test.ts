75ae1d35d35758a456f924906b3a4fa03658fbe6
Tomas Pilar <thomas7pilar@gmail.com>
2023-05-29 12:51:10 +0200

    chore(tests): migrate selected e2e test to integration
    Signed-off-by: Tomas Pilar <thomas7pilar@gmail.com>

--- src/tests/integration/client.test.ts
+++ src/tests/integration/client.test.ts
@@ -1,63 +1,122 @@
-import { MOCK_ENDPOINT } from '../mocks/handlers.js';
+import {
+  MOCK_ENDPOINT,
+  generateStore,
+  modelsStore,
+  tokenizeStore,
+} from '../mocks/handlers.js';
 import { Client } from '../../client.js';
 
 describe('client', () => {
   let client: Client;
-  beforeAll(() => {
+  beforeEach(() => {
     client = new Client({
       endpoint: MOCK_ENDPOINT,
       apiKey: 'foobar',
     });
   });
 
-  describe('generate config', () => {
-    test('should read the config', async () => {
-      const config = await client.generateConfig();
-      expect(config).toMatchObject({ model_id: 'foobar' });
+  describe('generate', () => {
+    describe('config', () => {
+      test('should read the config', async () => {
+        const config = await client.generateConfig();
+        expect(config).toMatchObject({ model_id: 'foobar' });
+      });
+
+      test('should replace the config', async () => {
+        const input = {
+          model_id: 'google/ul2',
+          parameters: {
+            decoding_method: 'greedy',
+            random_seed: 8,
+          },
+        };
+        const config = await client.generateConfig(input, {
+          strategy: 'replace',
+        });
+        expect(config).toMatchObject(input);
+      });
+
+      test('should merge the config', async () => {
+        const input = {
+          parameters: {
+            decoding_method: 'greedy',
+            random_seed: 8,
+          },
+        };
+        const config = await client.generateConfig(input, {
+          strategy: 'merge',
+        });
+        expect(config).toMatchObject({ model_id: 'foobar', ...input });
+      });
+
+      test('should set and reset the config', async () => {
+        const input = {
+          model_id: 'google/ul2',
+          parameters: {
+            decoding_method: 'greedy',
+            random_seed: 8,
+          },
+        };
+        const replacedConfig = await client.generateConfig(input, {
+          strategy: 'replace',
+        });
+        expect(replacedConfig).toMatchObject(input);
+
+        const config = await client.generateConfig({ reset: true });
+        expect(config).toMatchObject({ model_id: 'foobar' });
+      });
     });
 
-    test('should replace the config', async () => {
-      const input = {
-        model_id: 'google/ul2',
-        parameters: {
-          decoding_method: 'greedy',
-          random_seed: 8,
-        },
-      };
-      const config = await client.generateConfig(input, {
-        strategy: 'replace',
+    test('should return single output for a single input', async () => {
+      const data = await client.generate({
+        model_id: 'bigscience/bloom',
+        input: 'Hello, World',
       });
-      expect(config).toMatchObject(input);
+
+      expect(data).toMatchObject(generateStore);
+    }, 15_000);
+
+    test('should return multiple outputs for multiple inputs', async () => {
+      const inputs = [
+        {
+          model_id: 'bigscience/bloom',
+          input: 'Hello, World',
+        },
+        {
+          model_id: 'bigscience/bloom',
+          input: 'Hello again',
+        },
+      ];
+
+      const outputs = await Promise.all(client.generate(inputs));
+      expect(outputs.length).toBe(inputs.length);
+      outputs.forEach((output) => {
+        expect(output).toMatchObject(generateStore);
+      });
+    }, 20_000);
+  });
+
+  describe('tokenize', () => {
+    test('should return tokenize info', () => {
+      expect(
+        client.tokenize({
+          input: 'Hello, how are you? Are you okay?',
+          model_id: 'google/flan-t5-xl',
+        }),
+      ).resolves.toMatchObject(tokenizeStore);
+    });
+  });
+
+  describe('models', () => {
+    test('should return some models', async () => {
+      const models = await client.models();
+      expect(models.length).toBeGreaterThan(0);
     });
 
-    test('should merge the config', async () => {
-      const input = {
-        parameters: {
-          decoding_method: 'greedy',
-          random_seed: 8,
-        },
-      };
-      const config = await client.generateConfig(input, {
-        strategy: 'merge',
-      });
-      expect(config).toMatchObject({ model_id: 'foobar', ...input });
-    });
-
-    test('should set and reset the config', async () => {
-      const input = {
-        model_id: 'google/ul2',
-        parameters: {
-          decoding_method: 'greedy',
-          random_seed: 8,
-        },
-      };
-      const replacedConfig = await client.generateConfig(input, {
-        strategy: 'replace',
-      });
-      expect(replacedConfig).toMatchObject(input);
-
-      const config = await client.generateConfig({ reset: true });
-      expect(config).toMatchObject({ model_id: 'foobar' });
+    test('should return details for a given model', async () => {
+      const id = modelsStore[0].id;
+      const details = await client.model({ id });
+      expect(details).toHaveProperty('id', id);
     });
   });
 });
--- src/tests/mocks/handlers.ts
+++ src/tests/mocks/handlers.ts
@@ -10,13 +10,58 @@ export const resetGenerateConfigStore = () => {
   };
 };
 
+export const generateStore = {
+  generated_text: 'foobar',
+  generated_token_count: 1,
+  input_token_count: 5,
+  stop_reason: 'TOKEN_LIMIT',
+};
+
+export const tokenizeStore = {
+  token_count: 2,
+  tokens: ['foo', 'bar'],
+};
+
+export const modelsStore = [
+  {
+    id: 'foo/model',
+    name: 'Foo model',
+    size: '31B',
+    token_limit: 123,
+    tags: [],
+    source_model_id: null,
+    tasks: [],
+    model_family: {
+      id: 1,
+      name: 'foobar',
+    },
+    schema_generate: { id: 1, value: 'generate schema placeholder' },
+    schema_tokenize: { id: 1, value: 'tokenize schema placeholder' },
+  },
+  {
+    id: 'bar/model',
+    name: 'Bar model',
+    size: '15B',
+    token_limit: 456,
+    tags: [],
+    source_model_id: null,
+    tasks: [],
+    model_family: {
+      id: 1,
+      name: 'foobar',
+    },
+    schema_generate: { id: 1, value: 'generate schema placeholder' },
+    schema_tokenize: { id: 1, value: 'tokenize schema placeholder' },
+  },
+];
+
 export const resetStores = () => {
   resetGenerateConfigStore();
 };
 resetStores();
 
 export const handlers: RestHandler<MockedRequest<DefaultBodyType>>[] = [
-  // Generate Config Mocks
+  // Generate Config
   rest.get(`${MOCK_ENDPOINT}/v1/generate/config`, (req, res, ctx) =>
     res(ctx.status(200), ctx.json(generateConfigStore)),
   ),
@@ -31,5 +76,64 @@ export const handlers: RestHandler<MockedRequest<DefaultBodyType>>[] = [
   rest.patch(`${MOCK_ENDPOINT}/v1/generate/config`, async (req, res, ctx) => {
     generateConfigStore = _.merge({}, generateConfigStore, await req.json());
     return res(ctx.status(200), ctx.json(generateConfigStore));
+  }),
+
+  // Generate Limits
+  rest.get(`${MOCK_ENDPOINT}/v1/generate/limits`, (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json({
+        tokenCapacity: 100,
+        tokensUsed: 0,
+      }),
+    ),
+  ),
+
+  // Generate
+  rest.post(`${MOCK_ENDPOINT}/v1/generate`, async (req, res, ctx) => {
+    const body = await req.json();
+    return res(
+      ctx.status(200),
+      ctx.json({
+        results: new Array(body.inputs.length).fill(generateStore),
+      }),
+    );
+  }),
+
+  // Tokenize
+  rest.post(`${MOCK_ENDPOINT}/v1/tokenize`, async (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json({
+        results: [tokenizeStore],
+      }),
+    ),
+  ),
+
+  // Models
+  rest.get(`${MOCK_ENDPOINT}/v1/models`, async (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json({
+        results: modelsStore.map(({ id, name, size, token_limit }) => ({
+          id,
+          name,
+          size,
+          token_limit,
+        })),
+      }),
+    ),
+  ),
+  rest.get(`${MOCK_ENDPOINT}/v1/models/:id`, async (req, res, ctx) => {
+    const model = modelsStore.find((model) => model.id === req.params.id);
+    if (!model) {
+      return res(ctx.status(404));
+    }
+    return res(
+      ctx.status(200),
+      ctx.json({
+        results: model,
+      }),
+    );
   }),
 ];

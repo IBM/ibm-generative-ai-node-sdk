038cd154190d3bcaadef32c429051d6f8faaf003
Tomáš Dvořák <toomas2d@gmail.com>
2023-07-14 11:35:43 +0200

    feat(templates): add prompt templates conversion class (#33)

--- src/langchain/index.ts
+++ src/langchain/index.ts
@@ -1,2 +1,3 @@
 export * from './llm.js';
 export * from './llm-chat.js';
+export * from './prompt-template.js';
---
+++ src/langchain/prompt-template.ts
@@ -0,0 +1,43 @@
+import type { PromptTemplateOutput as PromptTemplate } from '../client-types.js';
+import { PromptTemplate as LangChainPromptTemplate } from 'langchain/prompts';
+import { InvalidInputError } from '../errors.js';
+
+export class GenAIPromptTemplate {
+  static toLangChain(
+    template: PromptTemplate | PromptTemplate['value'],
+  ): LangChainPromptTemplate {
+    const body = typeof template === 'string' ? template : template.value;
+    const fString = body.replace(
+      GenAIPromptTemplate.getTemplateMatcher('mustache'),
+      '{$1}',
+    );
+    return LangChainPromptTemplate.fromTemplate(fString, {
+      templateFormat: 'f-string',
+      validateTemplate: true,
+    });
+  }
+
+  static fromLangChain(
+    template: LangChainPromptTemplate,
+  ): PromptTemplate['value'] {
+    return template.template.replace(
+      GenAIPromptTemplate.getTemplateMatcher(template.templateFormat),
+      '{{$1}}',
+    );
+  }
+
+  private static getTemplateMatcher(name: string) {
+    switch (name) {
+      case 'mustache':
+        return /\{\{([^}]+)\}\}/g;
+      case 'jinja2':
+        return /\{\{\s*(.*?)\s*\}\}/g;
+      case 'fstring':
+      case 'f-string':
+        return /\{([^}]+)\}/g;
+      default: {
+        throw new InvalidInputError(`Unknown template format "${name}".`);
+      }
+    }
+  }
+}

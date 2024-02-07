9928aaeece7e107ab4c21916fe959cd3aadac8b1
Tomas Pilar <tomas.pilar@ibm.com>
2024-02-07 16:13:17 +0100

    fixup! concurrency error
    Signed-off-by: Tomas Pilar <tomas.pilar@ibm.com>

--- src/utils/concurrency.ts
+++ src/utils/concurrency.ts
@@ -1,7 +1,15 @@
 import PQueue, { QueueAddOptions } from 'p-queue';
 
+import { HttpError } from '../errors.js';
+
 type Limiter = () => Promise<{ limit: number }>;
 type Task<T> = () => Promise<T>;
+
+function isConcurrencyLimitError(err: unknown): err is HttpError {
+  return (
+    err instanceof HttpError && err.extensions?.code === 'TOO_MANY_REQUESTS'
+  );
+}
 
 export class ConcurrencyLimiter {
   private _queue?: PQueue;
@@ -10,7 +18,15 @@ export class ConcurrencyLimiter {
 
   async execute<T>(task: Task<T>, options?: Partial<QueueAddOptions>) {
     if (!this._queue) this._queue = await this.createQueue();
-    return this._queue.add(task, { ...options, throwOnTimeout: true });
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      try {
+        return this._queue!.add(task, { ...options, throwOnTimeout: true });
+      } catch (err) {
+        if (isConcurrencyLimitError(err)) continue;
+        throw err;
+      }
+    }
   }
 
   private async createQueue(): Promise<PQueue> {
